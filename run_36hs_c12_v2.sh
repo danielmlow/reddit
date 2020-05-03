60378e471a0010c0f179df30f8f4ba0ec165cc84
Daniel Mark Low <daniel.m.low@gmail.com>
2020-05-03 16:36:50 -0300

    added bash script v2

---
+++ run_36hs_c12_v2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=gs_24hs
+#SBATCH --array=1-5
+#SBATCH --time=36:00:00
+#SBATCH -N 1
+#SBATCH -c 12
+#SBATCH --mem=5GB
+#SBATCH -o ./../../datum/reddit/slurm/slurm-%A_%a.out
+
+echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
+
+python3 run.py --job_array_task_id=${SLURM_ARRAY_TASK_ID} --run_version_number=2 --toy=False
+
+echo 'Finished.'
+
+# tesla-k20:2
+# GEFORCEGTX1080TI:4
+# --gres=gpu:1
+
+# https://hpc-uit.readthedocs.io/en/latest/jobs/examples.html
