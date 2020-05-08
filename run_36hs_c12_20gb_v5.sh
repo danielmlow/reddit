7b3ee57af3a7665c466ebf00f1fd4a009775d046
Daniel Mark Low <daniel.m.low@gmail.com>
2020-05-07 23:11:29 -0300

    made 2 hs run time

--- run_36hs_c12_20gb_v5.sh
+++ run_36hs_c12_20gb_v5.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name=gs_v5
 #SBATCH --array=1-13
-#SBATCH --time=10:00:00
+#SBATCH --time=2:00:00
 #SBATCH -N 1
 #SBATCH -c 8
-#SBATCH --mem=25GB
+#SBATCH --mem=20GB
 #SBATCH -o ./../../datum/reddit/output/slurm/slurm-%A_%a.out
 
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
