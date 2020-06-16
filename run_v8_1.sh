95dbf7777bea6a5f8561b000a37f6ff23dc02e24
Daniel Mark Low <daniel.m.low@gmail.com>
2020-06-16 18:34:06 -0300

    classification v8 with 2700 dataset

--- run_v8_1.sh
+++ run_v8_1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=gs_v6
-#SBATCH --array=1-13
+#SBATCH --job-name=reddit
+#SBATCH --array=1-14
 #SBATCH --time=1:00:00
 #SBATCH -N 1
 #SBATCH -c 1
@@ -10,11 +10,10 @@
 
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
 
-python3 run.py --job_array_task_id=${SLURM_ARRAY_TASK_ID} --run_version_number=6 --toy=False --dim_reduction=False --run_modelN=2
+python3 run.py --job_array_task_id=${SLURM_ARRAY_TASK_ID} --run_version_number=8 --toy=False --dim_reduction=False --run_modelN=1
 
 echo 'Finished.'
 
-# python3 -i run.py --job_array_task_id=1 --run_version_number=6 --toy=True --dim_reduction=False --run_modelN=0
 # tesla-k20:2
 # GEFORCEGTX1080TI:4
 # --gres=gpu:1
